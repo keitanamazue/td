975af3dee90541c60a0447c01fee64c8d6dcc22b
keita namazue <keitanamazue@gmail.com>
2021-09-08 14:32:38 +0900

    responsive

--- components/Input.jsx
+++ components/Input.jsx
@@ -4,7 +4,7 @@ export const Input = (props) =>{
 
     return(
         <div class="flex items-center bg-green-200 p-4">
-            <input class="px-2 ml-5 mr-5 outline-none" type="text" value={todoText} onChange={onChangeTodoText}/>
+            <input class="px-2 md:ml-5 mr-5 outline-none" type="text" value={todoText} onChange={onChangeTodoText}/>
             <button class="rounded-xl bg-gray-300 text-gray-600 px-4" onClick={onClickAdd}>Add</button>
         </div>
     )
